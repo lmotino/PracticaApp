3f17cf9aa121c10005300c9a78eda885a244b426
Luis <lmotinoaguas@gmail.com>
2020-02-19 11:17:39 -0600

    Terminando de hacer el CRUD

--- bckApp/routes/api/seguridad/seguridad.js
+++ bckApp/routes/api/seguridad/seguridad.js
@@ -17,17 +17,31 @@ var userModel = require('./seguridad.model');
 // Crear un modelo de datos para la entidad
 
 // CRUD
-// http://localhost:3000/api/seguridad/users/all
+// http://localhost:3000/api/seguridad/user/all
 // Obtener todos los registros de usuarios
 router.get('/user/all', (req, res)=>{
     return res.status(200).json(userModel.getAll());
 }); //get user/all
 
-// http://localhost:3000/api/seguridad/users/1
+// http://localhost:3000/api/seguridad/user/1
 router.get('/user/:id',(req, res)=>{
     var id = parseInt( req.params.id );
     var user = userModel.getById(id);
     return res.status(200).json(user);
 });
+
+//http://localhots:3000/api/seguridad/user/new
+router.post('/user/new', (req, res)=>{
+    var datosEnviados = req.body;
+    var newUser = userModel.addNew(datosEnviados);
+    return res.status(200).json(newUser);
+}); // post user/new
+
+router.put('/user/upd/:id',(req, res)=>{
+    var id = parseInt(req.params.id);
+    var updUser = userModel.update(id, req.body);
+    res.status(200).json(updUser);
+});
+
 
 module.exports = router;
